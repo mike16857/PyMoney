5b87b3aac723b818bc5a34d89879b0b153503476
Mike Hu <mikehu980@gmail.com>
2023-10-23 02:42:57 +0800

    refine delete function

--- pymoney.py
+++ pymoney.py
@@ -2,13 +2,15 @@ money = int(input('How much money do you have? '))
 
 flag = True
 entries = []
+tmp = []
 
 while flag:
     action = input('What do you want to do (add / view / delete / exit)? ')
 
     if action == 'add':
-        entry = input('Add an expense or income record with description and amount:\ndesc1 amt1, desc2 amt2, desc3 amt3, ...\n').split(',')
-        for i in entry:
+        entry_to_add = input('Add an expense or income record with description and amount:\ndesc1 amt1, desc2 amt2, desc3 amt3, ...\n').split(',')
+
+        for i in entry_to_add:
             entries.append(tuple(i.split()))
             money += int(i.split()[1])
         # print(entries)
@@ -16,33 +18,45 @@ while flag:
 
     elif action == 'view':
         print('Here are your expense and income records:')
-        print('Description     Amount')
-        print('=============== ======')
-        for item, amount in entries:
-            print(f'{item:15} {amount:6}')
-        print('=============== ======')
+        print('Index    Description     Amount')
+        print('=====    ==============  ======')
+
+        for i, (item, amount) in enumerate(entries):
+            print(f'{i:<5d}    {item:15s} {int(amount):<6d}')
+
+        print('=====    ==============  ======')
         print(f'Now you have {money} dollars.\n')
         
     elif action == 'delete':
-        entry_to_delete = tuple(input('Enter an expense or income record you want to delete with description and amount:\n').split())
+        entry_to_delete = tuple(input('Enter an expense or income record you want to delete with description and amount: ').split())
+
         if entry_to_delete not in entries:
             print('Record not found.')
         else:
-            num = entries.count(entry_to_delete)
-            if num == 1:
+            for i in range(len(entries)):
+                if entries[i] == entry_to_delete: 
+                    tmp += [i]
+
+            if len(tmp) == 1:
                 entries.remove(entry_to_delete)
                 money -= int(entry_to_delete[1])
+
             else:
-                print(f'{num} identical records found.')
-                n = int(input('How many records do you want to delete? '))
-                if n > num:
+                print(f'{len(tmp)} identical records found. Which are')
+                for i in tmp:
+                    print(f'Entry #{i}')
+
+                n = int(input('Which one do you want to delete? '))
+
+                while (n not in tmp):
                     print('Invalid input, please try again.')
+                    n = int(input('Which one do you want to delete? '))
                 else:
-                    for i in range(n):
-                        entries.remove(entry_to_delete)
-                        money -= int(entry_to_delete[1])
-        print(entries)
-        print(money)
+                    del(entries[n])
+                    money -= int(entry_to_delete[1])
+        
+        tmp = []
+            
 
     elif action == 'exit':
         flag = False
@@ -50,12 +64,3 @@ while flag:
 
     else:
         print('Invalid input, please try again.')
-
-    # item, change = input('Add an expense or income record with   description and amount:\n').split()
-    # change = int(change)
-    # if item == 'q':
-    #     flag = False
-    #     break
-    # balance = money + change
-    # print('Now you have %d dollars.' %(balance))
-    # money = balance
