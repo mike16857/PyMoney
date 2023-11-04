f0fac1b698c55f87901a79e01cbcc9a650788cf8
Mike Hu <mikehu980@gmail.com>
2023-11-05 03:31:29 +0800

    week 7, 8 complete

--- pymoney.py
+++ pymoney.py
@@ -9,24 +9,59 @@ def initialize():
         init_money = int(fh.readline())
         st = ''
         tmp2 = fh.readlines()
-        for tp in tmp2:
-            st += tp
-        rcd = [eval(s) for s in st.split('\n')]
+        if not tmp2:    
+            rcd = []
+        else:
+            for tp in tmp2:
+                st += tp
+            rcd = [eval(s) for s in st.split('\n')]
         fh.close()
+    
+    except FileNotFoundError:  # No original record file
+        try:
+            init_money = int(input('How much money do you have? '))
 
-    except FileNotFoundError:
-        init_money = int(input('How much money do you have? '))
+        except ValueError:  # User inputs a string that cannot be converted to integer
+            sys.stderr.write('Invalid value for money. Set to 0 by default.\n') 
+            init_money = 0
+
+        rcd = []
+
+    except:  # Record in the file does not match the format
+        sys.stderr.write('Invalid format in records.txt. Deleting the contents.\n')
+        open('records.txt','w').close()
+
+        try:
+            init_money = int(input('How much money do you have? '))
+
+        except ValueError:  # User inputs a string that cannot be converted to integer
+            sys.stderr.write('Invalid value for money. Set to 0 by default.\n') 
+            init_money = 0
+
+        rcd = []
 
     return init_money, rcd
+
 
 def add(init_money, rcd):
-    entry_to_add = input('Add an expense or income record with description and amount:\ndesc1 amt1, desc2 amt2, desc3 amt3, ...\n').split(',')
-    for i in entry_to_add:
-        rcd.append(tuple(i.split()))
-        init_money += int(i.split()[1])
-    
-    return init_money, rcd
+    original_rcd = rcd[:]
+    original_init_money = init_money
+    try:
+        entry_to_add = input('Add an expense or income record with description and amount:\ndesc1 amt1, desc2 amt2, desc3 amt3, ...\n').split(',')
+        for i in entry_to_add:
+            rcd.append(tuple(i.split()))
+            init_money += int(i.split()[1])
 
+        return init_money, rcd
+
+    except IndexError:  # Input string does not follow the format
+        sys.stderr.write('The format of a record should be like this: breakfast -50.\nFail to add a record.\n')
+        return original_init_money, original_rcd
+    
+    except ValueError:  # The second string of a record, after splitting, cannot be converted to an integer
+        sys.stderr.write('Invalid value for money.\nFail to add a record.\n')
+        return original_init_money, original_rcd 
+    
 
 def view(init_money, rcd):
     print('Here are your expense and income records:')
@@ -41,11 +76,19 @@ def view(init_money, rcd):
 
 
 def delete(init_money, rcd):
+    if len(rcd) == 0:   # Check if there is record (ADDED FUNCTION)
+        print('Currently no record, go and add some records.')
+        return init_money, rcd
+
     tmp = []
     entry_to_delete = tuple(input('Enter an expense or income record you want to delete with description and amount: ').split())
 
-    if entry_to_delete not in rcd:
-        print('Record not found.')
+    if len(entry_to_delete) != 2:
+        print('Invalid format. Fail to delete a record.')  # Input format is invalid
+
+    elif entry_to_delete not in rcd:
+        print('Record not found. Fail to delete a record.')  # The specified record does not exist
+
     else:
         for i in range(len(rcd)):
             if rcd[i] == entry_to_delete: 
@@ -63,7 +106,9 @@ def delete(init_money, rcd):
             n = int(input('Which one do you want to delete? '))
 
             while (n not in tmp):
-                print('Invalid input, please try again.')
+                if n == -1: # Quit delete mode (ADDED FUNCTION)
+                    break
+                print('Invalid input, please try again.')   # The specified record does not exist 
                 n = int(input('Which one do you want to delete? '))
             else:
                 del(rcd[n])
@@ -96,6 +141,7 @@ while True:
 
     else:
         sys.stderr.write('Invalid command. Try again.\n')
+
 
 #region
 # entries = []
